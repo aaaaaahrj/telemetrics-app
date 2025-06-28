ac7a58fcf9b1776c35e15fe19c443535d4782fbd
Arj Tabudlong <108502620+aaaaaahrj@users.noreply.github.com>
2025-06-28 17:09:03 +0800

    Update page.tsx

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "@/components/authentication/login-form";
-export default function PagePage() {
+export default function Page() {
   return (
     <div className='bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10'>
       <div className='w-full max-w-sm md:max-w-3xl'>
